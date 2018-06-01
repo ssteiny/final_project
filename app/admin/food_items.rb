51d6f584c54c19b6c2c8c5809a629a120f358892
Raghu Betina <raghu@firstdraft.com>
2018-06-01 23:53:35 +0000

    Whitelist columns in FoodItem for ActiveAdmin

--- app/admin/food_items.rb
+++ app/admin/food_items.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register FoodItem do
+
+ permit_params :name, :image, :price
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
