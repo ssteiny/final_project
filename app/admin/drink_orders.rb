9b8fe5948ccd8370502b52db632a48dd775b488d
Raghu Betina <raghu@firstdraft.com>
2018-06-01 23:53:43 +0000

    Whitelist columns in DrinkOrder for ActiveAdmin

--- app/admin/drink_orders.rb
+++ app/admin/drink_orders.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register DrinkOrder do
+
+ permit_params :drink_id, :bar_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
