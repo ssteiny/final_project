a594be64e5dc71d07fc8e492afeb642fdf82e26a
Raghu Betina <raghu@firstdraft.com>
2018-06-01 23:53:18 +0000

    Direct association between bars and food_orders

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,6 +1,9 @@
 class Bar < ApplicationRecord
   # Direct associations
 
+  has_many   :food_orders,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/food_order.rb
+++ app/models/food_order.rb
@@ -1,6 +1,8 @@
 class FoodOrder < ApplicationRecord
   # Direct associations
 
+  belongs_to :bar
+
   # Indirect associations
 
   # Validations
