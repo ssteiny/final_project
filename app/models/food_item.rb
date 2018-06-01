75a11a9a1547e4d947bd7268357eba07554fcedc
Raghu Betina <raghu@firstdraft.com>
2018-06-01 23:53:18 +0000

    Direct association between food_items and food_orders

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,6 +1,10 @@
 class FoodItem < ApplicationRecord
   # Direct associations
 
+  has_many   :food_orders,
+             :foreign_key => "food_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/food_order.rb
+++ app/models/food_order.rb
@@ -1,6 +1,9 @@
 class FoodOrder < ApplicationRecord
   # Direct associations
 
+  belongs_to :food,
+             :class_name => "FoodItem"
+
   belongs_to :bar
 
   # Indirect associations
